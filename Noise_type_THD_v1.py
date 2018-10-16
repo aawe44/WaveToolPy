0218b09e547c6ec7277410b3fae3de7987b16d5c
JasonChen <aawe44@gmail.com>
2018-10-16 18:08:20 +0800

    update noise_type: add select_channel and num_of_noise

--- Noise_type_THD_v1.py
+++ Noise_type_THD_v1.py
@@ -1,9 +1,12 @@
+#2018/10/16 add select channel and num_of_noise
+
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
 
 def endpoint_dect(wave_data):
-    # split the duration of noise 
+
+    # split the duration of noise
     diff=[]
     flag=0
 
@@ -27,49 +30,63 @@ def endpoint_dect(wave_data):
             startpoint=np.append(startpoint,i)
         if x[i]<0:
             endpoint=np.append(endpoint,i)
-          
+    '''
+    print(startpoint,endpoint)
+    print(len(startpoint),len(endpoint))
+    print(type(wave_data))
+    print(len(wave_data))
+    '''
+    if len(startpoint) ==(len(endpoint)+1):
+        endpoint = np.append(endpoint, len(wave_data))
+
     output=np.vstack((startpoint,endpoint))
     return output
 # end of  endpoint_dect
 #==================================================
 
-file = r"D:\Work\20180718_Recording_Crash_QA\ReleaseTest\check_no_sound\01_vptx_out.HSWB.TS.tuned.wav"
-#file = r"D:\Work\20180718_Recording_Crash_QA\ReleaseTest\check_no_sound\vptx_out.is880.B3.04.wav"
+file = "vptx_out.is880.20181016.01.wav"
+num_of_noise=4
 
 f = wave.open(file, "rb")
-
 params = f.getparams()
 nchannels, sampwidth, framerate, nframes = params[:4]
 
-'''
 print ("Frame Rate: " +  str(framerate))
 print ("Channel Number: " +  str(nchannels))
 print ("Sample Width: " +  str(sampwidth))
 print ("Sample frames: " +  str(nframes))
-'''
+
 
 str_data = f.readframes(nframes)
 f.close()
 
-str_data = np.fromstring(str_data, dtype=np.short)
+str_data = np.frombuffer(str_data, dtype=np.short)
 str_data.shape = -1, nchannels
 str_data = str_data.T
 
 time = np.arange(0, nframes) * (1.0 / framerate)
 
+#print(str_data.shape)
+
 all_channel_val=[]
 wave_data=[]
 
-for i in range(3,7):
+
+#define select channels: [High, LowHigh, High2, High3]
+select_channels=range(5,9)
+
+for i in select_channels:
     
     wave_data=str_data[i]
-    #print(endpoint_dect(wave_data)/framerate)
-    
+#    print(endpoint_dect(wave_data)/framerate)
+
     endpoint=endpoint_dect(wave_data)
     endpoint=np.array(endpoint,dtype=np.int)
 
     channel_val=[]
     temp=[]
+
+    #print(endpoint.shape)
 
     for val in endpoint.T:
 
@@ -78,16 +95,19 @@ for i in range(3,7):
 
         wav_max=max(wav_duration)
         wav_min=min(wav_duration)
-        
+
+        #print(wav_max, wav_min)
+
         temp=np.vstack((wav_max,wav_min))
         channel_val=np.append(channel_val,temp)
 
     all_channel_val=np.append(all_channel_val,channel_val)
 
-print(all_channel_val.reshape(4,8))
+print(all_channel_val.reshape(4,num_of_noise*2))
 print('Done !!!')
 
-np.savetxt('Noise_type_THD.txt',all_channel_val.reshape(4,8),fmt='%d',delimiter=' ')
+np.savetxt('Noise_type_THD.txt',all_channel_val.reshape(4,num_of_noise*2),fmt='%d',delimiter=' ')
+
 
 
 
